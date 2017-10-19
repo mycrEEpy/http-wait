122407317464ccb50a99c0e4bb97665893d61f7f
Tobias <bvrcreepyx@hotmail.de>
2017-10-19 21:34:55 +0200

    use milliseconds instead of seconds

--- main.go
+++ main.go
@@ -2,36 +2,43 @@ package main
 
 import (
 	"fmt"
-	"log"
 	"net/http"
 	"os"
 	"strconv"
-
 	"time"
 
 	"github.com/gorilla/mux"
 )
 
+var (
+	hostname string
+)
+
 func main() {
+	var err error
+	hostname, err = os.Hostname()
+	if err != nil {
+		fmt.Print("Hostname not found\n")
+		os.Exit(1)
+	}
 	r := mux.NewRouter()
-	r.HandleFunc("/{seconds}", handler)
+	r.HandleFunc("/{millis}", handler)
 	http.Handle("/", r)
-	log.Println("Listening on 8080")
-	log.Fatal(http.ListenAndServe(":8080", r))
+	fmt.Printf("Listening on 8080\nHit me with curl %s:8080/<milliseconds>\n", hostname)
+	err = http.ListenAndServe(":8080", r)
+	if err != nil {
+		fmt.Printf("HTTP listen and serve: %s\n", err)
+		os.Exit(1)
+	}
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
 	vars := mux.Vars(r)
-	seconds, err := strconv.Atoi(vars["seconds"])
+	millis, err := strconv.ParseInt(vars["millis"], 10, 64)
 	if err != nil {
 		fmt.Fprintf(w, "NaN")
 		return
 	}
-	hostname, err := os.Hostname()
-	if err != nil {
-		fmt.Fprintf(w, "Hostname not found")
-		return
-	}
-	time.Sleep(time.Second * time.Duration(seconds))
-	fmt.Fprintf(w, "Hello from %v! I have waited for %v seconds", hostname, seconds)
+	time.Sleep(time.Millisecond * time.Duration(millis))
+	fmt.Fprintf(w, "Hello from %s! I have waited for %d milliseconds\n", hostname, millis)
 }
